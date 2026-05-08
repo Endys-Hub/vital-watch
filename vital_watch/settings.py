18dbe50a263ddce9a7400ce125a257246ac9474a
Ndudi <ndudiokehi@gmail.com>
2026-05-08 14:19:03 +0100

    Configured postgres database

--- vital_watch/settings.py
+++ vital_watch/settings.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,10 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'vital_watch.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
